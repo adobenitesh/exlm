3270e7e6c28208b12add41935919ad5c7c72601e
Nitesh Pandey <adobenitesh@gmail.com>
2023-12-04 18:53:04 +0530

    profile update

--- scripts/data-service/profile.js
+++ scripts/data-service/profile.js
@@ -1,7 +1,7 @@
 import { loadIms } from '../../scripts/scripts.js';
 import { signOut } from '../../scripts/auth/auth-operations.js';
 import { JWT, Organization, Profile, ProfileAttributes } from '../auth/session-keys.js';
-import { request } from '../request.js';
+import { request, headerKeys } from '../request.js';
 
 export const lang = document.querySelector('html').lang;
 export const origin = "https://experienceleague.adobe.com/";
