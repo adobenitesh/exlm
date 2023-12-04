f5d7bc605c53a64c54a05b47634ca103c619f2c1
Nitesh Pandey <adobenitesh@gmail.com>
2023-12-04 15:22:59 +0530

    profile update

--- scripts/data-service/profile.js
+++ scripts/data-service/profile.js
@@ -1,5 +1,7 @@
 import { signOut } from '../auth/auth-operations.js';
+import { csrf } from '../auth/csrf.js';
 import { keys as headerKeys, values as headerValues, JWT, Organization, Profile, ProfileAttributes} from '../auth/session-keys.js';
+import { JWTTokenUrl } from '../urls.js';
 
 const lang = document.querySelector('html').lang;
 const origin = 'https://experienceleague.adobe.com/';
@@ -9,7 +11,7 @@ let profileData = null;
 
 async function profileAttributes () {
   if (ProfileAttributes in sessionStorage === false) {
-    const res = await request(profileUrl, {
+    const res = await request(`${origin}${profileUrl}=?${lang}`, {
       credentials: 'include',
       headers: {
         [headerKeys.auth]: sessionStorage.getItem(JWT),
@@ -95,4 +97,62 @@ export async function profile (reuse = false, cstream = true, explicit = false) 
   }
 
   return result;
+}
+
+export async function updateProfile (key, val, replace = false) {
+  const data = await profile(false, false, true);
+  console.log("hello after profile method");
+
+  if (Reflect.ownKeys(meta).length === 0) {
+    meta = await profileAttributes();
+    console.log("hello after profile attributes");
+  }
+
+  Reflect.ownKeys(data).forEach(i => {
+    if (meta.write.includes(i) === false) {
+      delete data[i];
+    }
+  });
+
+  if (override.test(key) || replace === true) {
+    data[key] = val;
+  } else if (meta.types[key] === 'array') {
+    if (data[key] === void 0 || replace === true) {
+      data[key] = [val];
+    } else if (Array.isArray(data[key]) === false) {
+      data[key] = [data[key], val];
+    } else {
+      (Array.isArray(val) ? val : [val]).forEach(arg => {
+        if (data[key].includes(arg) === false) {
+          data[key].push(arg);
+        } else {
+          data[key].splice(data[key].indexOf(arg), 1);
+        }
+      });
+    }
+  } else {
+    data[key] = val;
+  }
+
+  const res = await request(`${origin}${profileUrl}=?${lang}`, {
+    method: 'PATCH',
+    credentials: 'include',
+    headers: {
+      authorization: sessionStorage.getItem(JWT),
+      accept: 'application/json',
+      'content-type': 'application/json-patch+json',
+      'x-csrf-token': await csrf(JWTTokenUrl)
+    },
+    body: JSON.stringify([{op: 'replace', path: `/${key}`, value: data[key]}])
+  });
+
+  if (res.ok && res.status < 400) {
+    const arg = await res.json();
+
+    profileData = await profileMerge(arg.data);
+    await profile(true);
+    sessionStorage.setItem(Profile, JSON.stringify(profileData));
+  }
+
+  return profileData;
 }
