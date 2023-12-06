4950ae5fbb9d03843653f9bfb2a6e5fe7205dc4a
Nitesh Pandey <adobenitesh@gmail.com>
2023-12-06 20:11:38 +0530

    update profile

--- blocks/doc-actions/doc-actions.js
+++ blocks/doc-actions/doc-actions.js
@@ -90,18 +90,18 @@ export function decorateBookmark(block) {
               bookmarkAuthedToolTipLabel.innerHTML = CONFIG.BOOKMARK_AUTH_LABEL_SET;
               bookmarkAuthedToolTipIcon.classList.remove('authed');
               sendNotice(CONFIG.BOOKMARK_UNSET);
+              bookmarkAuthed.style.pointerEvents = "none";
             } else {
               await updateProfile('bookmarks', id);
               bookmarkAuthedToolTipLabel.innerHTML = CONFIG.BOOKMARK_AUTH_LABEL_REMOVE;
               bookmarkAuthedToolTipIcon.classList.add('authed');
               sendNotice(CONFIG.BOOKMARK_SET);
+              bookmarkAuthed.style.pointerEvents = "none";
             }
-            bookmarkAuthed.style.pointerEvents = "none";
+            setTimeout(() => {
+              bookmarkAuthed.style.pointerEvents = "auto";
+            }, 3000);
           });
-
-          setTimeout(() => {
-            bookmarkAuthed.style.pointerEvents = "auto";
-          }, 3000);
         });
       }
     }
